28e090b3acb1d37a2b23c303462ab8768ede0405
tomalbrc <tomalbrc@me.com>
2025-04-29 23:53:01 +0200

    fix disintegrator

--- src/main/java/de/tomalbrc/microfighters/item/DisintegratorItem.java
+++ src/main/java/de/tomalbrc/microfighters/item/DisintegratorItem.java
@@ -2,20 +2,24 @@ package de.tomalbrc.microfighters.item;
 
 import de.tomalbrc.microfighters.entity.Fighter;
 import eu.pb4.polymer.core.api.item.PolymerItem;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
+import net.minecraft.world.item.component.TooltipDisplay;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import xyz.nucleoid.packettweaker.PacketContext;
 
-import java.util.List;
 import java.util.Objects;
+import java.util.function.Consumer;
 
 public class DisintegratorItem extends Item implements PolymerItem {
     public DisintegratorItem(Properties properties) {
@@ -25,6 +29,11 @@ public class DisintegratorItem extends Item implements PolymerItem {
     @Override
     public Item getPolymerItem(ItemStack itemStack, PacketContext context) {
         return Items.ENDER_EYE;
+    }
+
+    @Override
+    public @Nullable ResourceLocation getPolymerItemModel(ItemStack stack, PacketContext context) {
+        return this.getPolymerItem(stack, context).components().get(DataComponents.ITEM_MODEL);
     }
 
     @Override
@@ -49,8 +58,9 @@ public class DisintegratorItem extends Item implements PolymerItem {
         return Component.literal("Micro Fighter Disintegrator");
     }
 
-    public void appendHoverText(ItemStack itemStack, TooltipContext tooltipContext, List<Component> list, TooltipFlag tooltipFlag) {
-        list.add(Component.literal("Disintegrates all Micro Fighters"));
-        list.add(Component.literal("in a 16 block radius"));
+    @Override
+    public void appendHoverText(ItemStack itemStack, TooltipContext tooltipContext, TooltipDisplay tooltipDisplay, Consumer<Component> consumer, TooltipFlag tooltipFlag) {
+        consumer.accept(Component.literal("Disintegrates all Micro Fighters"));
+        consumer.accept(Component.literal("in a 16 block radius"));
     }
 }
